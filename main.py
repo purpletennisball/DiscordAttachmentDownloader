aef5fff210c908fe1a6380527f8fbaa212521089
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2025-05-20 12:26:15 -0400

    Progress Bar fixed
    Wasnt working at all on mac before and gave a warning on windows but now its fixed yayyy
    
    Fixes #7

--- main.py
+++ main.py
@@ -1,9 +1,86 @@
 # Imports
-import json, os, requests, ffmpeg, datetime, sys, threading
+import json, os, requests, ffmpeg, datetime, sys
+from PyQt5.QtCore import QObject, pyqtSignal, QThread
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton, QProgressBar, QHBoxLayout, QFileDialog
 from PyQt5.QtGui import QFont
 
 titleFont = QFont("Arial", 22)
+
+class Downloader(QObject):
+    progressChanged = pyqtSignal(int)
+    finished = pyqtSignal()
+
+    def run(self):
+        folder = window.messagesFolder.text()
+        exportFolder = window.exportFolder.text()
+        self.progressChanged.emit(0)
+        print("Checking messages for attachments...")
+        attachmentList = []
+        channelFolders = os.listdir(folder)
+        m4aConvert = False
+        audioTypes = ["ogg", "m4a", "mp3", "wav", "aac"]
+        for channelFolder in channelFolders:
+            if channelFolder != "index.json" or channelFolder != ".DS_Store":
+                messageFileLocation = folder + "/" + channelFolder + "/messages.json"
+                if os.path.exists(messageFileLocation):
+                    with open(messageFileLocation, "r", encoding="utf-8") as file:
+                        messages = json.load(file)
+                        for message in messages:
+                            if message["Attachments"]:
+                                for attachment in message["Attachments"].split(" "):
+                                    originalName = attachment.split("/")[-1].split("?")[0]
+                                    attachmentList.append(
+                                        {
+                                            "url": attachment,
+                                            "date": message["Timestamp"],
+                                            "channel": channelFolder,
+                                            "originalName": originalName, 
+                                            "id": message["ID"]
+                                        }
+                                    )
+        print("Finished")
+
+        attachmentCount = len(attachmentList)
+        attachmentsDownloaded = 0
+        print(f"Downloading {attachmentCount} Attachements")
+
+        for attachment in attachmentList:
+            percent = int(attachmentsDownloaded/attachmentCount*100)
+            self.progressChanged.emit(percent)
+            fileName = f"{exportFolder}/discord {attachment["channel"]} m{attachment["id"]} {attachment["originalName"]}"
+            newFileName = f"{fileName}.m4a"
+            with open(fileName, "wb") as file:
+                fail = False
+                if not (os.path.exists(newFileName)):
+                    # Download file
+                    attachmentContents = requests.get(attachment["url"])
+                    file.write(attachmentContents.content)
+                    # Convert to m4a
+                    if m4aConvert:
+                        try:
+                            if not fileName.endswith("m4a"):
+                                ffmpeg.input(fileName).output(newFileName).run()
+                            else:
+                                with open(newFileName, "wb") as file2:
+                                    file2.write(attachmentContents.content)
+                        except:
+                            fail = True
+                if not fail:
+                    # Remove Original File
+                    if m4aConvert: os.remove(fileName)
+                    # Set time of file to message timestamp
+                    date, time = attachment["date"].split(" ")
+                    year, month, day = date.split("-")
+                    hour, minute, second = time.split(":")
+                    time = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
+                    timestamp = time.timestamp()
+                    if m4aConvert: 
+                        os.utime(newFileName, (timestamp, timestamp))
+                    else:
+                        os.utime(fileName, (timestamp, timestamp))
+            attachmentsDownloaded = attachmentsDownloaded + 1
+        self.progressChanged.emit(100)
+        self.finished.emit()
 
 class FolderSelect(QWidget):
     def __init__(self):
@@ -54,80 +131,19 @@ class App(QMainWindow):
         self.setCentralWidget(self.widget)
 
     def __download(self):
-        threading.Thread(target=self._download, daemon=False).start()
+        self.thread = QThread()
+        self.worker = Downloader()
+        self.worker.moveToThread(self.thread)
 
-    def _download(self):
-        self.download(self.messagesFolder.text(), self.exportFolder.text())
-    
-    def download(self, folder, exportFolder):
-        self.bar.setValue(0)
-        print("Checking messages for attachments...")
-        attachmentList = []
-        channelFolders = os.listdir(folder)
-        m4aConvert = False
-        audioTypes = ["ogg", "m4a", "mp3", "wav", "aac"]
-        for channelFolder in channelFolders:
-            if channelFolder != "index.json" or channelFolder != ".DS_Store":
-                messageFileLocation = folder + "/" + channelFolder + "/messages.json"
-                if os.path.exists(messageFileLocation):
-                    with open(messageFileLocation, "r", encoding="utf-8") as file:
-                        messages = json.load(file)
-                        for message in messages:
-                            if message["Attachments"]:
-                                for attachment in message["Attachments"].split(" "):
-                                    originalName = attachment.split("/")[-1].split("?")[0]
-                                    attachmentList.append(
-                                        {
-                                            "url": attachment,
-                                            "date": message["Timestamp"],
-                                            "channel": channelFolder,
-                                            "originalName": originalName, 
-                                            "id": message["ID"]
-                                        }
-                                    )
-        print("Finished")
-
-        attachmentCount = len(attachmentList)
-        attachmentsDownloaded = 0
-        print(f"Downloading {attachmentCount} Attachements")
-
-        for attachment in attachmentList:
-            percent = int(attachmentsDownloaded/attachmentCount*100)
-            self.bar.setValue(percent)
-            fileName = f"{exportFolder}/discord {attachment["channel"]} m{attachment["id"]} {attachment["originalName"]}"
-            newFileName = f"{fileName}.m4a"
-            with open(fileName, "wb") as file:
-                fail = False
-                if not (os.path.exists(newFileName)):
-                    # Download file
-                    attachmentContents = requests.get(attachment["url"])
-                    file.write(attachmentContents.content)
-                    # Convert to m4a
-                    if m4aConvert:
-                        try:
-                            if not fileName.endswith("m4a"):
-                                ffmpeg.input(fileName).output(newFileName).run()
-                            else:
-                                with open(newFileName, "wb") as file2:
-                                    file2.write(attachmentContents.content)
-                        except:
-                            fail = True
-                if not fail:
-                    # Remove Original File
-                    if m4aConvert: os.remove(fileName)
-                    # Set time of file to message timestamp
-                    date, time = attachment["date"].split(" ")
-                    year, month, day = date.split("-")
-                    hour, minute, second = time.split(":")
-                    time = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
-                    timestamp = time.timestamp()
-                    if m4aConvert: 
-                        os.utime(newFileName, (timestamp, timestamp))
-                    else:
-                        os.utime(fileName, (timestamp, timestamp))
-            attachmentsDownloaded = attachmentsDownloaded + 1
-        self.bar.setValue(100)
-
+        self.thread.started.connect(self.worker.run)
+        self.worker.progressChanged.connect(self.bar.setValue)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        
+        self.thread.start()
+        self.startButton.setEnabled(False)
+        self.thread.finished.connect(lambda: self.startButton.setEnabled(True))
 
 app = QApplication(sys.argv)
 window = App()
